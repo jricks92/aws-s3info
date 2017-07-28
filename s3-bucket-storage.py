2abeac9f9b881f3966ce81774fd0c007d744da44
Jameson Ricks <jameson@glenricks.com>
2017-07-28 09:46:42 -0600

    Added multi-threading and better human readable output

--- s3-bucket-storage.py
+++ s3-bucket-storage.py
@@ -1,6 +1,13 @@
+#!/usr/local/bin/python3
+
+# Original script taken from http://www.slsmk.com/getting-the-size-of-an-s3-bucket-using-boto3-for-aws/
+# Modified by Jameson Ricks, Jul 28, 2017
+# Added human readable output, thread concurrency.
+
+
 import boto3
 import datetime
-from hurry.filesize import size # For printing out file sizes in human readable format
+import concurrent.futures
 
 now = datetime.datetime.now()
 
@@ -17,35 +24,60 @@ print('Bucket'.ljust(f_col_width) + 'Size in Bytes'.rjust(25))
 
 # Array of different S3 Storage types
 storage_types = ['StandardStorage', 'StandardIAStorage', 'ReducedRedundancyStorage', 'GlacierObjectOverhead']
+suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'] # For human readable
 
 # Keep running total
 total = 0
 
-# Iterate through each bucket
-for bucket in allbuckets['Buckets']:
+# Prints human readable sizes (source: https://stackoverflow.com/questions/14996453/python-libraries-to-calculate-human-readable-filesize-from-bytes)
+def humansize(nbytes):
+    i = 0
+    while nbytes >= 1024 and i < len(suffixes) - 1:
+        nbytes /= 1024.
+        i += 1
+    f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
+    return '%s %s' % (f, suffixes[i])
+
+# Function definition for getting all info
+def get_bucket_storage(bucket):
+    # get global total
+    global total
     # For each bucket item, look up the cooresponding metrics from CloudWatch
     for st_type in storage_types:
-        response = cw.get_metric_statistics(Namespace='AWS/S3',
-                                            MetricName='BucketSizeBytes',
-                                            Dimensions=[
-                                                {'Name': 'BucketName', 'Value': bucket['Name']},
-                                                {'Name': 'StorageType', 'Value': st_type},
-                                            ],
-                                            Statistics=['Average'],
-                                            Period=3600,
-                                            StartTime=(now-datetime.timedelta(days=1)).isoformat(),
-                                            EndTime=now.isoformat()
-                                            )
-        # The cloudwatch metrics will have the single datapoint, so we just report on it. 
-        for item in response["Datapoints"]:
-            bucket_name = bucket["Name"] + " (" + st_type + ")"
-            # print(bucket_name.ljust(50) + str("{:,}".format(int(item["Average"]))).rjust(25))
-            # size(1024)
-            print(bucket_name.ljust(f_col_width) + size(item["Average"]).rjust(25))
-            # Note the use of "{:,}".format.   
-            # This is a new shorthand method to format output.
-            # I just discovered it recently. 
-            total += int(item["Average"])
+            response = cw.get_metric_statistics(Namespace='AWS/S3',
+                                                MetricName='BucketSizeBytes',
+                                                Dimensions=[
+                                                    {'Name': 'BucketName',
+                                                        'Value': bucket['Name']},
+                                                    {'Name': 'StorageType',
+                                                        'Value': st_type},
+                                                ],
+                                                Statistics=['Average'],
+                                                Period=3600,
+                                                StartTime=(
+                                                    now - datetime.timedelta(days=1)).isoformat(),
+                                                EndTime=now.isoformat()
+                                                )
+            # The cloudwatch metrics will have the single datapoint, so we just report on it.
+            for item in response["Datapoints"]:
+                bucket_name = bucket["Name"] + " (" + st_type + ")"
+                # print(bucket_name.ljust(50) + str("{:,}".format(int(item["Average"]))).rjust(25))
+                # size(1024)
+                print(bucket_name.ljust(f_col_width) +
+                      humansize(item["Average"]).rjust(25))
+                # Note the use of "{:,}".format.
+                # This is a new shorthand method to format output.
+                # I just discovered it recently.
+                total += int(item["Average"])
 
-print("\n\nTotal stored in S3:".ljust(f_col_width) + size(total).rjust(25))
-print("Total bytes stored in S3:".ljust(f_col_width) + str("{:,}".format(int(item["Average"]))).rjust(25) + " bytes")
+# Use multi-threading to make it faster
+with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executer:
+    future_to_bucket = {executer.submit(get_bucket_storage, bucket): bucket for bucket in allbuckets['Buckets']}
+    # Iterate through each bucket
+    # for future in concurrent.futures.as_completed(future_to_bucket):
+    #     pass
+    # for bucket in allbuckets['Buckets']:
+    #     get_bucket_storage(bucket)
+
+print("\n\nTotal stored in S3:".ljust(f_col_width) + humansize(total).rjust(25))
+print("Total bytes stored in S3:".ljust(f_col_width) + str("{:,}".format(int(total))).rjust(25) + " bytes")
