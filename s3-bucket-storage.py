610887e93934ad471eedf62c710443c6cbdcb1d0
Jameson Ricks <jameson@glenricks.com>
2017-07-28 11:47:03 -0600

    Added help info and safe check for boto3 install

--- s3-bucket-storage.py
+++ s3-bucket-storage.py
@@ -5,9 +5,18 @@
 # Added human readable output, thread concurrency.
 
 import sys
-import boto3
 import datetime
 import concurrent.futures
+
+try:
+    import boto3
+except ImportError, e:
+    print('''
+ERROR:  You must have the boto3 package installed in your python environment to run 
+        this script! You can install it by running:
+            pip install boto3
+        ''')
+    sys.exit(1)
 
 
 ## Global variables
@@ -109,7 +118,39 @@ def get_bucket_storage(bucket, session):
                 total += int(item["Average"])
 
 def print_help():
-    print("This is the help message for now...")
+    print('''usage: ./s3-bucket-storage.py [-h | --help] [-v | --verbose] [--profile=<profile>]
+                            [--workers=<# of threads>] [--single-thread] [--raw-bytes] 
+    ''')
+
+    print('''DESCRIPTION
+Use this tool to output your total S3 usage in an AWS account. It can display your total usage
+in human readable format (e.g., KB, MB, GB, TB, PB) or in total bytes. You can display usage per 
+bucket according to type of storage (Standard, Infrequently Accessed, Reduced Redundancy, or
+Glacier Objects). Buckets that have multiple types of storage will be listed twice. This tool 
+uses concurrent threads to speed up the process. As such, buckets will not be listed
+in alphabetical order unless you run with the --single-thread flag. By default, this tool uses 
+the default profile stored in your ~/.aws/config file.
+
+NOTE:   You must have the boto3 package installed in your python environment to correctly run this
+        script.    
+
+        -h OR --help            Shows this help message.
+
+        -v OR --verbose         Outputs the size of each bucket
+
+        --profile=<profile>     Uses the specified profile stored in your ~/.aws/config file.
+
+        --workers=<number>      Specifies a specific number of threads to parse through each S3
+                                bucket (default is 10). More threads may speed up the process if
+                                you have a large number of S3 buckets in your account.abs
+
+        --single-thread         Runs this script using one thread. This is useful if you want to
+                                see all your buckets output in alphabetical order. Using this
+                                flag will take longer to loop through all your S3 buckets.
+
+        --raw-bytes             Using this option, you can output each bucket size in bytes
+                                instead of KB, MB, GB, or PB.
+    ''')
 
 
 ## Main function
